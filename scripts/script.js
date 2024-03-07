5bdfd4b1f81bb3c2515041e3d4a0eff0ca3d6e33
Robert Thao <robert.lee.thao@gmail.com>
2024-03-07 17:30:55 -0600

    added a header along with a prompt asking the user if they're okay with viewing animation on the DOM

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ let menu = {
     appetizer: "",
     mainCourse: ""
 };
-console.log(menu);
+// console.log(menu);
 
 // 2 - Assign values to the properties using the dot notation 
 //(e.g., "Espresso", "Bruschetta", "Salmon Steak")
@@ -16,14 +16,14 @@ console.log(menu);
 
 // 3 - Create a variable called `specials` and assign it to an empty object literal
 let specials = {};
-console.log(specials);
+// console.log(specials);
 
 // 4 - Add properties to `specials` using the bracket notation for the following specials: 
 //"Drink Special", "Appetizer Special", "Main Course Special"
 specials["Drink Special"] = '';
 specials["Appetizer Special"] = '';
 specials["Main Course Special"] = '';
-console.log(specials);
+// console.log(specials);
 
 // 5 - Assign special items (e.g., "Blue Note Cocktail", "Jazz Trio Sampler", "Miles Davis Ribeye") 
 //to each special using the dot notation
@@ -34,16 +34,16 @@ console.log(specials);
 
 // 6 - Create an empty array called `playlist`
 let playlist = [];
-console.log(playlist);
+// console.log(playlist);
 
 // 7 - Use the push() method to add at least 3 jazz songs to the `playlist` array (e.g., "Take Five", 
 //"So What", "My Favorite Things")
 playlist.push("Take Five", "So What", "My Favorite Things");
-console.log(playlist);
+// console.log(playlist);
 
 // 8 - Use the pop() method to remove the last song from the `playlist` array
 playlist.pop();
-console.log(playlist);
+// console.log(playlist);
 
 // 9 - Create a variable called `totalSongs` and set it equal to the length of the `playlist` array
 let totalSongs = playlist.length;
@@ -57,7 +57,7 @@ console.log(roundedTotalSongs);
 // 11 - Create a variable called `daysRemaining` to show the days remaining for the special menu items
 //and set it to 7
 let daysRemaining = 7;
-
+// console.log(daysRemaining);
 
 // 12 - Create a while loop that decrements `daysRemaining` by 1 each iteration, and console logs the 
 //current value of `daysRemaining`
@@ -73,21 +73,35 @@ let message = "";
 // 14 - Use concat() to concatenate the strings "Welcome to our jazz cafe! Our menu features " with the 
 //value of the `mainCourse` property from `menu`
 message = message.concat("Welcome to our jazz cafe! Our menu features ", menu.mainCourse);
-console.log(message);
+// console.log(message);
 
 // 15 - Use concat() to concatenate the string ", and our specials include " with the value of the 
 //"Drink Special" property from `specials`
 message = message.concat(", and our specials include ", specials["Drink Special"]);
-console.log(message);
+// console.log(message);
 
 // 16 - Use concat() to concatenate the string ", along with our playlist of " with the value of the 
 //`totalSongs` variable
 message = message.concat(", along with our playlist of ", totalSongs);
-console.log(message);
+// console.log(message);
 
 // 17 - Use toUpperCase() to convert the entire `message` string to uppercase
 message = message.toUpperCase();
-console.log( message );
+// console.log( message );
 
 // 18 - Console log the `message` string
 console.log(message);
+
+// playground - develop a animated banner that scrolls the message from left to right
+let playAnimation = false;
+// yes or no user prompt upon page load - prompting "are you okay with viewing animation?"
+window.addEventListener("load", function accessiblePrompt(){
+    if(confirm("Hello! Are you okay with viewing an animation?")){
+        playAnimation = true;
+        console.log( playAnimation );
+    }else {
+        console.log("No animation, got it!");
+    };
+
+}
+);
